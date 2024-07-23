f1b3030b4342ff18fa15ed0646383ccc0284acc4
psgtm <85763617+psgtm@users.noreply.github.com>
2024-07-23 22:33:42 +0530

    SymmetricTree

---
+++ SymmetricTree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        return self.isSameTree(root.left,root.right)
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.right) and self.isSameTree(p.right, q.left)
