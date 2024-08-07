abb5e20056344725a1c41547a122d90fe9c0aab9
psgtm <85763617+psgtm@users.noreply.github.com>
2024-08-08 01:28:32 +0530

    MaximumBinaryTree

---
+++ MaximumBinaryTree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums):
+        if not nums:
+            return None
+        
+        max_index = nums.index(max(nums))
+        
+        root = TreeNode(nums[max_index])
+        
+        root.left = self.constructMaximumBinaryTree(nums[:max_index])
+        root.right = self.constructMaximumBinaryTree(nums[max_index + 1:])
+        
+        return root
